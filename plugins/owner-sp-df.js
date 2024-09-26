224770b64b67ce87c708adfc0fbe72e753ba4579
YuuraHz <159578677+Adixshnzz@users.noreply.github.com>
2024-09-26 18:01:49 +0700

    Update owner-sp-df.js

--- plugins/owner-sp-df.js
+++ plugins/owner-sp-df.js
@@ -1,21 +1,21 @@
-let fs = require('fs');
+let fs = require('fs')
 module.exports = {
    run: async (m, {
       text,
       usedPrefix,
       command
 }) => {
-  if (!text) throw `uhm.. teksnya mana?`;
+  if (!text) return m.reply(Func.example(usedPrefix, command, 'reply code!'))
   if (command === 'sp') {
-    if (!m.quoted.text) throw `balas pesan nya!`;
-    let path = `plugins/${text}.js`;
-    await fs.writeFileSync(path, m.quoted.text);
-    m.reply(`tersimpan di ${path}`);
+    if (!m.quoted.text) return m.reply('balas pesan nya!')
+    let path = `plugins/${text}.js`
+    await fs.writeFileSync(path, m.quoted.text)
+    m.reply(`tersimpan di ${path}`)
   } else if (command === 'df') {
-    let path = `plugins/${text}.js`;
-    if (!fs.existsSync(path)) throw `file plugin ${text}.js tidak ditemukan`;
-    await fs.unlinkSync(path);
-    m.reply(`file plugin ${text}.js berhasil dihapus`);
+    let path = `plugins/${text}.js`
+    if (!fs.existsSync(path)) return m.reply(`file plugin ${text}.js tidak ditemukan`)
+    await fs.unlinkSync(path)
+    m.reply(`file plugin ${text}.js berhasil dihapus`)
   }
 },
   help: ['df'],
